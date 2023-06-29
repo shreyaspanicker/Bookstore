34bdcf7122fe0da5904d9d2ddb0439ba845ae0ac
Shreyas Panicker <panickershreyas@gmail.com>
2023-06-30 03:37:58 +0530

    Refine search feature for the app
    • Added dropdown menu for searching based on categories
    • Added message component for the app
    • Refactoring of existing code

--- src/state/store.js
+++ src/state/store.js
@@ -7,11 +7,22 @@ const store = new Vuex.Store({
   state: {
     header: {
       title: "Bookstore",
-      tagline: "The store you can't afford to miss",
+      tagline: "The store you can't afford to miss.",
+    },
+    searchParams: {
+      category: "",
+      keywords: [],
     },
   },
-  actions: {},
-  mutations: {},
+  mutations: {
+    setSearchParams(state, value) {
+      state.searchParams.category = value.category;
+      state.searchParams.keywords = value.keywords;
+    },
+  },
+  getters: {
+    getSearchParams: (state) => state.searchParams,
+  },
 });
 
 export default store;
